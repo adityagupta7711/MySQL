79244a6565d0202c1c5e7504e1a5d1569ef161f1
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-25 21:45:22 +0530

    new file

---
+++ insertValues.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES
+
+(4, "Jay", "jay@gmail.com", "123456789","89329828","plot 41, gzb", "1999-01-10", "F", 1),
+(5, "raj", "raj@gmail.com", "123456789","89329828","plot 41, gzb", "1999-01-10", "M", 1)
